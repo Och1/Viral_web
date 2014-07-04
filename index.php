135737f99850d04b58d6f3f2f7b4ab8aa8da0e5c
och1 <och.a@sorako.mn>
2014-07-04 18:44:44 +0800

    viral source init xiilee

---
+++ index.php
@@ -0,0 +1,77 @@
+<!DOCTYPE html>
+<head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <meta charset="utf-8">
+    <meta name="description" content="">
+
+    <link href="style/style.css" rel="stylesheet" type="text/css">
+
+    <title>Page</title>
+
+
+
+<body id="article">
+<?php include_once("analyticstracking.php") ?>
+<div id="wrap">
+
+    <div id="contents">
+        <div id="contInner">
+            <div class="loadBox">
+                <div class="loadItem">
+                    <div class="clearfix">
+                        <div id="articleCont" class="fLeft">
+                            <div id="articleWrap">
+                                <section id="titleArea">
+
+                                    <h1>Title</h1>
+
+                                </section>
+
+
+                                <figure class="top20">
+
+                                    <p><img src="images/1.jpg" width="600" alt=""></p>					</figure>
+
+                                <p class="text top10">Test</p>
+                                <h2 class="top40">test1</h2>
+                                <figure class="top20">
+                                    <p>
+                                        <iframe width="560" height="315" src="//www.youtube.com/embed/MSgnf-tBN9s" frameborder="0" allowfullscreen></iframe>
+                                    </p>
+                                </figure>
+                                <p class="text top10">test</p>
+                            </div><!--articleWrap-->
+
+
+
+                        </div><!--articleCont-->
+
+                        <section class="popularRank fRight">
+                            test test test testtest testtest testtest testtest testtest testtest testtest testtest test test testtest testtest testtest testtest test
+                            test test test testtest testtest testtest testtest testtest testtest testtest testtest test test testtest testtest testtest testtest test
+                            test test test testtest testtest testtest testtest testtest testtest testtest testtest test test testtest testtest testtest testtest test
+                            test test test testtest testtest testtest testtest testtest testtest testtest testtest test test testtest testtest testtest testtest test
+
+                        </section>
+                    </div>
+
+                </div>
+
+
+            </div>
+
+        </div>
+    </div>
+
+</div>
+<script>
+    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+        (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+        m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+    })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+    ga('create', 'UA-52544511-1', 'auto');
+    ga('send', 'pageview');
+
+</script>
+</body>
+</html>
